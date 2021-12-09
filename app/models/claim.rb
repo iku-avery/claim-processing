b922140295c528df69ce1514150cebcc85b2394a
Ika Grabon <ika.grabon@lunarlogic.io>
2021-12-09 18:22:47 +0100

    Add validations to models

--- app/models/claim.rb
+++ app/models/claim.rb
@@ -1,4 +1,8 @@
 class Claim < ApplicationRecord
   has_many :iterinaries, dependent: :destroy
   has_many :flights, through: :iterinaries
+
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+
+  enum state: { in_progress: 0, rejected: 1, eligible: 2 }, _default: :in_progress
 end
